b17ac250ec8f6a610455bf4f41fac52aad66b74c
Easton Kang <euisungkang@gmail.com>
2024-05-23 23:00:04 +0900

    feat: config settings

---
+++ src/main/java/spinai/photoviewerbackend/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package spinai.photoviewerbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    @Value("${jwt.signing.secret}")
+    private String jwtSecret;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/token/**").permitAll()
+                        .requestMatchers("/api/v1/users").permitAll()
+                        .requestMatchers("/api/v1/users/validation/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] key = jwtSecret.getBytes();
+        SecretKey originalKey = new SecretKeySpec(key, 0, key.length, "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(originalKey)
+                .macAlgorithm(MacAlgorithm.HS256).build();
+    }
+}
